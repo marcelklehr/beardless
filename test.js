5408149077344836fb98c731d2632a2a8abc4fa5
Marcel Klehr <mklehr@gmx.net>
2012-10-21 14:16:25 +0200

    Migrate tests to vows

--- test.js
+++ test.js
@@ -1,32 +1,46 @@
-﻿var dreamer = require('./index')
+var beardless = require('./index')
   , path = require('path')
   , fs = require('fs')
+  , vows = require('vows')
   , assert = require('assert')
   , testPath = __dirname+'/test/'
 
-var tests = {}
+var tests = { /* testName => { html=> '...the template ...', json => '...params..', out => '... html output ...' } */}
+// load all tests
 fs.readdirSync(testPath).forEach(function(filename) {
-  console.log('loading '+filename+' ...')
   var test = filename.split('.')[0]
     , data = filename.split('.')[1]
   if(!tests[test]) tests[test] = {}
   tests[test][data] = fs.readFileSync(path.join(testPath, filename)).toString()
 })
 
-console.log('Testing...\n')
+var suite = vows.describe('beardless')
 
 for (var test in tests) {
-  var template = new dreamer
-    , testData = tests[test]
-  
-  template.insert(JSON.parse(testData.json))
-  
-  template.render(testData.html, function(err, out) {
-  try{
-    if(assert.ifError(err)) return console.log(err);
-    
-    assert.equal(out, testData.out)
-    console.log('Passed test', test)
-  }catch(e){console.log(test+' failed: ', e)}
-  })
+  (function (test) {
+    var batch = {}
+    batch[test] = {
+      topic: function() {
+        var template = new beardless
+          , testData = tests[test]
+        this.testData = testData
+        
+        try{
+          template.insert(JSON.parse(testData.json))
+          template.render(testData.html, this.callback)
+        }catch(e) {
+          this.callback(e)
+        }
+      },
+      'should render the template correctly': function(err, out) {
+        assert.ifError(err)
+        assert.equal(out, this.testData.out)
+      }
+    }
+    suite.addBatch(batch)
+  })(test)
 }
+
+suite.run(null, function() {
+  (suite.results.broken+suite.results.errored) > 0 && process.exit(1)
+})
